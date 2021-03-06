9682ec27d1a314a349a5a78fab4118cf59b5a5ff
Henrique Mauri <hgmauri@gmail.com>
2021-03-06 17:10:57 -0300

    removing using

--- src/Sample.Elasticsearch.WebApi.Core/Middleware/ErrorHandlingMiddleware.cs
+++ src/Sample.Elasticsearch.WebApi.Core/Middleware/ErrorHandlingMiddleware.cs
@@ -2,9 +2,7 @@
 using Newtonsoft.Json;
 using Serilog;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Sample.Elasticsearch.WebApi.Core.Middleware
--- src/Sample.Elasticsearch.WebApi.Core/Middleware/RequestSerilLogMiddleware.cs
+++ src/Sample.Elasticsearch.WebApi.Core/Middleware/RequestSerilLogMiddleware.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
 using Serilog.Context;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Sample.Elasticsearch.WebApi.Core.Middleware
