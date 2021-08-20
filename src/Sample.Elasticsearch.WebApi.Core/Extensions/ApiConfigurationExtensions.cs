5f4bbedd2d70efdf0d7f7eba15992d73932592b2
Henrique Mauri <hgmauri@gmail.com>
2021-08-20 06:38:04 -0300

    update startup

---
+++ src/Sample.Elasticsearch.WebApi.Core/Extensions/ApiConfigurationExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Compression;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nest;
+using Sample.Elasticsearch.Domain.Applications;
+using Sample.Elasticsearch.Domain.Interfaces;
+using Sample.Elasticsearch.Domain.Repositories;
+using Sample.Elasticsearch.WebApi.Core.HealthCheck;
+using Sample.Elasticsearch.WebApi.Core.Middleware;
+using Serilog;
+
+namespace Sample.Elasticsearch.WebApi.Core.Extensions
+{
+    public static class ApiConfigurationExtensions
+    {
+        public static void AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            services.AddTransient<IActorsApplication, ActorsApplication>();
+            services.AddTransient<IActorsRepository, ActorsRepository>();
+            services.AddScoped<IMyCustomService, MyCustomService>();
+
+            services.AddResponseCompression();
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+
+            services.AddControllers();
+        }
+
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSerilogRequestLogging(opts => opts.EnrichDiagnosticContext = SerilogExtensions.EnrichFromRequest);
+
+            app.UseMiddleware<RequestSerilLogMiddleware>();
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+        }
+    }
+}
--- src/Sample.Elasticsearch.WebApi/Startup.cs
+++ src/Sample.Elasticsearch.WebApi/Startup.cs
@@ -2,14 +2,9 @@ using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Sample.Elasticsearch.WebApi.Core.Extensions;
-using Sample.Elasticsearch.WebApi.Core.Middleware;
-using Serilog;
-using System.IO.Compression;
 
 namespace Sample.Elasticsearch.WebApi
 {
@@ -24,41 +19,18 @@ namespace Sample.Elasticsearch.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddApiConfiguration();
 
             services.AddElasticsearch(Configuration);
-            services.AddServices();
             services.AddSwagger(Configuration);
             services.AddHealthCheckApi(Configuration);
-
-            services.AddResponseCompression();
-            services.Configure<GzipCompressionProviderOptions>(options =>
-            {
-                options.Level = CompressionLevel.Optimal;
-            });
-
-            services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseSerilogRequestLogging(opts => opts.EnrichDiagnosticContext = SerilogExtensions.EnrichFromRequest);
-
-            app.UseMiddleware<RequestSerilLogMiddleware>();
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-                
+            app.UseApiConfiguration(env);
+            
             app.UseSwaggerDoc();
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseHealthCheckApi();
 
